eb10c30f6f60f70d839c301bcd7775f5b155c50a
VeleaFlorian <73984035+VeleaFlorian@users.noreply.github.com>
2024-01-08 20:06:45 +0200

    Local Solution

--- LocalSolution/DataLoader.py
+++ LocalSolution/DataLoader.py
@@ -1,12 +1,18 @@
+import glob
 from csv import reader
+
 class DataLoader:
-    def __init__(self, localtrain_path):
-        # TODO
-        pass
-        
-        
+    def __init__(self, directory_path):
+        self.directory_path = directory_path
+        self.localtrain_data = {}
+        self.csv_names = []
+
     def load_data(self):
-        # TODO
-        pass
-        
+        csv_files = glob.glob(self.directory_path + '/*.csv')
+        for path in csv_files:
+            self.csv_names.append(path)
+            with open(path, 'r') as read_obj:
+                csv_reader = reader(read_obj)
+                file_data = [row for row in csv_reader]
+                self.localtrain_data[path] = file_data
         
