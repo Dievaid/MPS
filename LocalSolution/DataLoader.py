71674ae4e0a22e0233a8a5862404e643127083b5
Bogdan-Brutaru <brutaru_bogdan11@yahoo.com>
2024-01-08 16:58:37 +0200

    Data Implementation

--- LocalSolution/DataLoader.py
+++ LocalSolution/DataLoader.py
@@ -2,11 +2,15 @@ from csv import reader
 class DataLoader:
     def __init__(self, localtrain_path):
         # TODO
-        pass
+        self._localtrain_path = localtrain_path
+        self.localtrain_data = [[]]
         
         
     def load_data(self):
         # TODO
-        pass
+        print("Loading LocalTrain.csv")
+        with open(self._localtrain_path) as file:
+            self.localtrain_data = list(reader(file, delimiter=','))[0]
+        print("Done with LocalTrain")
         
         
